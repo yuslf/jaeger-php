5bb7f353934d7ec84625fb8d9630d55cfa245b7f
满鸿 <manhong@xin.com>
2019-04-25 11:45:27 +0800

    ceshi

--- src/Jaeger/Script/EventHandler.php
+++ src/Jaeger/Script/EventHandler.php
@@ -1,13 +1,19 @@
 <?php
 namespace Jaeger\Script;
 
-//use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
+use Composer\Script\Event;
+use Composer\Script\PackageEvent;
 
 class EventHandler
 {
     public static function postPackageInstall(PackageEvent $event)
     {
+        echo '$event->getName()';
+        var_dump($event->getName());
+
+        echo '$event->getComposer()->getConfig()->get(vendor-dir)';
+        var_dump($event->getComposer()->getConfig()->get('vendor-dir'));
+
         $installedPackage = $event->getOperation()->getPackage();
 
         echo '$installedPackage->getName()';
@@ -22,6 +28,31 @@ class EventHandler
         var_dump($installedPackage->getSourceReference());
         echo '$installedPackage->getIncludePaths()';
         var_dump($installedPackage->getIncludePaths());
+    }
+
+    public static function test(Event $event)
+    {
+        echo '$event->getName()';
+        var_dump($event->getName());
+
+        echo '$event->getComposer()->getConfig()->get(vendor-dir)';
+        var_dump($event->getComposer()->getConfig()->get('vendor-dir'));
+
+        $installedPackage = $event->getComposer()->getPackage();
+
+        echo '$installedPackage->getName()';
+        var_dump($installedPackage->getName());
+        echo '$installedPackage->getPrettyName()';
+        var_dump($installedPackage->getPrettyName());
+        echo '$installedPackage->getTargetDir()';
+        var_dump($installedPackage->getTargetDir());
+        echo '$installedPackage->getInstallationSource()';
+        var_dump($installedPackage->getInstallationSource());
+        echo '$installedPackage->getSourceReference()';
+        var_dump($installedPackage->getSourceReference());
+        echo '$installedPackage->getIncludePaths()';
+        var_dump($installedPackage->getIncludePaths());
+
     }
 
 }
