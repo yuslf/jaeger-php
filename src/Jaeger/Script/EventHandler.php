cbf72ef19ac483eb3170377257c9cdff18fd4a72
满鸿 <manhong@xin.com>
2019-04-25 13:38:41 +0800

    ceshi

--- src/Jaeger/Script/EventHandler.php
+++ src/Jaeger/Script/EventHandler.php
@@ -8,50 +8,55 @@ class EventHandler
 {
     public static function postPackageInstall(PackageEvent $event)
     {
-        echo '$event->getName()';
-        var_dump($event->getName());
+        $io = $event->getIO();
 
-        echo '$event->getComposer()->getConfig()->get(vendor-dir)';
-        var_dump($event->getComposer()->getConfig()->get('vendor-dir'));
+        $io->write('$event->getName()');
+        $io->write($event->getName());
+
+        $io->write('$event->getComposer()->getConfig()->get(vendor-dir)');
+        $io->write($event->getComposer()->getConfig()->get('vendor-dir'));
 
         $installedPackage = $event->getOperation()->getPackage();
 
-        echo '$installedPackage->getName()';
-        var_dump($installedPackage->getName());
-        echo '$installedPackage->getPrettyName()';
-        var_dump($installedPackage->getPrettyName());
-        echo '$installedPackage->getTargetDir()';
-        var_dump($installedPackage->getTargetDir());
-        echo '$installedPackage->getInstallationSource()';
-        var_dump($installedPackage->getInstallationSource());
-        echo '$installedPackage->getSourceReference()';
-        var_dump($installedPackage->getSourceReference());
-        echo '$installedPackage->getIncludePaths()';
-        var_dump($installedPackage->getIncludePaths());
+        $io->write('$installedPackage->getName()');
+        $io->write($installedPackage->getName());
+        $io->write('$installedPackage->getPrettyName()');
+        $io->write($installedPackage->getPrettyName());
+        $io->write('$installedPackage->getTargetDir()');
+        $io->write($installedPackage->getTargetDir());
+        $io->write('$installedPackage->getInstallationSource()');
+        $io->write($installedPackage->getInstallationSource());
+        $io->write('$installedPackage->getSourceReference()');
+        $io->write($installedPackage->getSourceReference());
+        $io->write('$installedPackage->getIncludePaths()');
+        $io->write($installedPackage->getIncludePaths());
+
     }
 
     public static function test(Event $event)
     {
-        echo '$event->getName()';
-        var_dump($event->getName());
+        $io = $event->getIO();
 
-        echo '$event->getComposer()->getConfig()->get(vendor-dir)';
-        var_dump($event->getComposer()->getConfig()->get('vendor-dir'));
+        $io->write('$event->getName()');
+        $io->write($event->getName());
+
+        $io->write('$event->getComposer()->getConfig()->get(vendor-dir)');
+        $io->write($event->getComposer()->getConfig()->get('vendor-dir'));
 
         $installedPackage = $event->getComposer()->getPackage();
 
-        echo '$installedPackage->getName()';
-        var_dump($installedPackage->getName());
-        echo '$installedPackage->getPrettyName()';
-        var_dump($installedPackage->getPrettyName());
-        echo '$installedPackage->getTargetDir()';
-        var_dump($installedPackage->getTargetDir());
-        echo '$installedPackage->getInstallationSource()';
-        var_dump($installedPackage->getInstallationSource());
-        echo '$installedPackage->getSourceReference()';
-        var_dump($installedPackage->getSourceReference());
-        echo '$installedPackage->getIncludePaths()';
-        var_dump($installedPackage->getIncludePaths());
+        $io->write('$installedPackage->getName()');
+        $io->write($installedPackage->getName());
+        $io->write('$installedPackage->getPrettyName()');
+        $io->write($installedPackage->getPrettyName());
+        $io->write('$installedPackage->getTargetDir()');
+        $io->write($installedPackage->getTargetDir());
+        $io->write('$installedPackage->getInstallationSource()');
+        $io->write($installedPackage->getInstallationSource());
+        $io->write('$installedPackage->getSourceReference()');
+        $io->write($installedPackage->getSourceReference());
+        $io->write('$installedPackage->getIncludePaths()');
+        $io->write($installedPackage->getIncludePaths());
 
     }
 
